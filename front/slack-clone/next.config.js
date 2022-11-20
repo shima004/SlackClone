602729a89180ca638a27a3a265ea9fbdad2934e5
shima004 <yellow030408@gmail.com>
2022-11-20 13:47:52 +0000

    set environment

--- front/slack-clone/next.config.js
+++ front/slack-clone/next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+  reactStrictMode: true,
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 800,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
+};
