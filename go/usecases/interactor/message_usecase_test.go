46deb2857421c02c2c36a69e4a951681c8d16dc9
shima004 <yellow030408@gmail.com>
2023-06-28 17:40:10 +0900

    add validation test

--- go/usecases/interactor/message_usecase_test.go
+++ go/usecases/interactor/message_usecase_test.go
@@ -56,6 +56,24 @@ func TestGetAllMessages(t *testing.T) {
 		assert.Error(t, err)
 		assert.Nil(t, list)
 	})
+
+	t.Run("should return validation error", func(t *testing.T) {
+		t.Parallel()
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+
+		invalidInput := &validation.FatchMessagesInput{
+			ChannelID: 1,
+			Limit:     -1,
+			Offset:    -1,
+		}
+
+		mu := DefaultMessageUsecase{}
+		list, err := mu.FetchMessages(context.TODO(), invalidInput)
+		assert.Error(t, err)
+		assert.EqualError(t, err, "limit must be greater than 0\noffset must be greater than 0\n")
+		assert.Nil(t, list)
+	})
 }
 
 func TestPostMessage(t *testing.T) {
@@ -100,6 +118,21 @@ func TestPostMessage(t *testing.T) {
 		err := mu.PostMessage(context.Background(), in)
 		assert.Error(t, err)
 	})
+
+	t.Run("should return validation error", func(t *testing.T) {
+		t.Parallel()
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+
+		invalidInput := &validation.PostMessageInput{
+			Message: &entities.Message{},
+		}
+
+		mu := DefaultMessageUsecase{}
+		err := mu.PostMessage(context.Background(), invalidInput)
+		assert.Error(t, err)
+		assert.EqualError(t, err, "text must not be empty\n")
+	})
 }
 
 func TestDeleteMessage(t *testing.T) {
@@ -117,6 +150,26 @@ func TestDeleteMessage(t *testing.T) {
 	}
 
 	t.Run("should return nil", func(t *testing.T) {
+		t.Parallel()
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+
+		mockRepository := mock_repository.NewMockMessageRepository(mockCtrl)
+		mockRepository.EXPECT().ReadMessage(gomock.Any(), mockMessage.ID).Return(&mockMessage, nil).Times(1)
+		// mockRepository.EXPECT().DeleteMessage(gomock.Any(), mockMessage.ID).Return(nil).Times(1)
+
+		invalidInput := &validation.DeleteMessageInput{
+			MessageID: mockMessage.ID,
+			UserID:    4536,
+		}
+
+		mu := DefaultMessageUsecase{MessageRepository: mockRepository}
+		err := mu.DeleteMessage(context.Background(), invalidInput)
+		assert.Error(t, err)
+		assert.EqualError(t, err, "unauthorized")
+	})
+
+	t.Run("should return unauthorized error", func(t *testing.T) {
 		t.Parallel()
 		mockCtrl := gomock.NewController(t)
 		defer mockCtrl.Finish()
@@ -150,5 +203,16 @@ func TestUpdateMessage(t *testing.T) {
 		mu := DefaultMessageUsecase{MessageRepository: mockRepository}
 		err := mu.UpdateMessage(context.Background(), in)
 		assert.NoError(t, err)
+	})
+
+	t.Run("should return validation error", func(t *testing.T) {
+		invalidInput := &validation.UpdateMessageInput{
+			Message: &entities.Message{},
+		}
+
+		mu := DefaultMessageUsecase{}
+		err := mu.UpdateMessage(context.Background(), invalidInput)
+		assert.Error(t, err)
+		assert.EqualError(t, err, "text must not be empty\n")
 	})
 }
